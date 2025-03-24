323a04cfee74f29a1e14efb87d2943ede2d08349
GeorgiySav <gesav@live.com>
2025-03-24 18:53:48 +0000

    merged theming to main

--- sustainableCampus/settings.py
+++ sustainableCampus/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import dj_database_url
+import pymysql
+pymysql.install_as_MySQLdb()
+
 from pathlib import Path
 from sustainableCampus.celery_settings import CELERY_BEAT_SCHEDULE
 
@@ -26,7 +30,7 @@ SECRET_KEY = 'django-insecure-n9q!h@er0pm+)jjuh&-i)4vp2x-m!f0ypd0-ts&+s9@q$6a@bu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sustainableCampus.pythonanywhere.com']
 
 
 # Application definition
@@ -65,7 +69,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
 SESSION_COOKIE_AGE = 1209600
 SESSION_SAVE_EVERY_REQUEST = True
 SESSION_COOKIE_SECURE = True
@@ -96,10 +104,19 @@ WSGI_APPLICATION = 'sustainableCampus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sustainableCampu$default',
+        'USER': 'sustainableCampu',
+        'PASSWORD': 'suscam123',
+        'HOST': 'sustainableCampus.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
+
 
 
 # Password validation
@@ -138,8 +155,20 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATIC_FILES_DIRS = os.path.join(BASE_DIR, 'static')
+
+
+# This production code might break development mode, so we check whether we're in DEBUG mode
+# if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -149,7 +178,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_FROM = 'sustainablecampusteam@gmail.com'
 EMAIL_HOST_USER = 'sustainablecampusteam@gmail.com'
-EMAIL_HOST_PASSWORD = 'tcayrclskdythofv' # App password
+EMAIL_HOST_PASSWORD = 'tcayrclskdythofv'  # App password
+
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
@@ -159,4 +189,13 @@ EMAIL_USE_TLS = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/accounts/login/'
 LOGOUT_REDIRECT_URL = '/'
+
 LOGIN_REDIRECT_URL = '/'
+
+#-----renderversion----
+# ALLOWED_HOSTS = [
+#     'mysite-veg7.onrender.com',  # domain name
+#     'localhost',
+#     '127.0.0.1',
+#     '[::1]'
+# ]
